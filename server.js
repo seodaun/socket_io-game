570416fb68c94ec206388849cc64277d020db833
seodaun <daun4799@naver.com>
2020-04-08 20:13:22 +0900

    test

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ io.on('connection', function (socket) {
     });  
     
 });
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 3000;
 http.listen(port, function () {
     console.log('server on!');
     console.log('http://localhost:'+port);
